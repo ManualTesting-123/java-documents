9266235bef0eecb51d2531b3ac1dae6943617d7b
ManualTesting-123 <87077883+ManualTesting-123@users.noreply.github.com>
2021-12-16 22:35:24 +0530

    Add files via upload

---
+++ arrthimetical_operators.java
@@ -0,0 +1,46 @@
+package java_programming;
+
+import java.util.Scanner;
+
+public class arrthimetical_operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+int y=20;
+int z;
+z=x+y;
+System.out.println(z);
+
+char c='a';
+char c1='b';
+int c2;
+c2=c1 + c;
+System.out.println(c2);
+//no concatination for characters 
+
+String b="balakrishna";
+String b1="kumar";
+String b2;
+b2=b+b1;
+System.out.println(b2);
+
+double m=90.90;
+double m1=90.0000;
+double m2;
+m2=m1+m;
+System.out.println(m2);
+
+boolean a1=true;
+System.out.println(a1);
+
+
+	
+	
+		
+	
+	
+
+	}
+
+}
